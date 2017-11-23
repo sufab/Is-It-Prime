c1b877e97a959a8f028c12d6313e919c051274de
sufa bitton <sufabitton@sufas-imac>
2017-11-23 17:09:11 +0200

    Initial Commit

---
+++ Is it prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Is it prime
+//
+//  Created by sufa bitton on 23/11/2017.
+//  Copyright © 2017 sufa bitton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
